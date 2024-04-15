5a7f313357adc55ced60f90edf1b33f2b3030c96
Samuell Torres <samuelltorres145@gmail.com>
2024-04-15 16:26:26 -0300

    feat: grafico de evolução financeira

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -5,6 +5,7 @@ import { Card } from '../../components/card';
 import { CategoriesPieChart } from '../../components/categories-pie-chart';
 import { CreateCategoryDialog } from '../../components/create-category-dialog';
 import { CreateTransactionDialog } from '../../components/create-transaction-dialog';
+import { FinancialEvolutionBarChart } from '../../components/financial-evolution-bar-chart';
 import { Input } from '../../components/input';
 import { Logo } from '../../components/logo';
 import { Title } from '../../components/title';
@@ -103,7 +104,9 @@ export function Home() {
                 <ButtonIcon />
               </ChartAction>
             </header>
-            <ChartContent></ChartContent>
+            <ChartContent>
+              <FinancialEvolutionBarChart />
+            </ChartContent>
           </ChartContainer>
         </Section>
         <Aside>
