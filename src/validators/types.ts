3c889928da67e6d768956c9a4811db76ab07d377
Samuell Torres <samuelltorres145@gmail.com>
2024-04-17 14:53:28 -0300

    feat(api): tipagem para o type de dado da criação de transação

--- src/validators/types.ts
+++ src/validators/types.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
-import { createCategorySchema } from './schemas';
+import { createCategorySchema, createTransactionSchema } from './schemas';
 
 export type CreateCategoryData = z.infer<typeof createCategorySchema>;
+
+export type CreateTransactionData = z.infer<typeof createTransactionSchema>;
