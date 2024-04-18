36c29ffd132caaaeaa8cb345dbc96a2691978dda
Samuell Torres <samuelltorres145@gmail.com>
2024-04-18 15:20:38 -0300

    feat(API): integração com grafico de evolucao financeira anual

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -20,7 +20,10 @@ import { Title } from '../../components/title';
 import { Transaction } from '../../components/transaction';
 import { useFetchAPI } from '../../hooks/useFetchAPI';
 import { transactionsFilterSchema } from '../../validators/schemas';
-import { TransactionsFilterData } from '../../validators/types';
+import {
+  FinancialEvolutionFilterData,
+  TransactionsFilterData,
+} from '../../validators/types';
 import {
   Aside,
   Balance,
@@ -48,15 +51,34 @@ export function Home() {
     resolver: zodResolver(transactionsFilterSchema),
   });
 
-  const { transactions, fetchTransactions, dashboard, fetchDashboard } =
-    useFetchAPI();
+  const financialEvolutionFilterForm = useForm<FinancialEvolutionFilterData>({
+    defaultValues: {
+      year: dayjs().get('year').toString(),
+    },
+  });
+
+  const {
+    transactions,
+    fetchTransactions,
+    dashboard,
+    fetchDashboard,
+    financialEvolution,
+    fetchFinancialEvolution,
+  } = useFetchAPI();
 
   useEffect(() => {
     const { beginDate, endDate } = transactionsFilterForm.getValues();
 
     fetchDashboard({ beginDate, endDate });
     fetchTransactions(transactionsFilterForm.getValues());
-  }, [fetchTransactions, transactionsFilterForm, fetchDashboard]);
+    fetchFinancialEvolution(financialEvolutionFilterForm.getValues());
+  }, [
+    fetchTransactions,
+    transactionsFilterForm,
+    fetchDashboard,
+    fetchFinancialEvolution,
+    financialEvolutionFilterForm,
+  ]);
 
   const [selectedCategory, setSelectedCategory] =
     useState<CategoryProps | null>(null);
@@ -94,6 +116,13 @@ export function Home() {
     },
 
     [fetchDashboard, fetchTransactions],
+  );
+
+  const onSubmitFinancialEvolution = useCallback(
+    async (data: FinancialEvolutionFilterData) => {
+      await fetchFinancialEvolution(data);
+    },
+    [fetchFinancialEvolution],
   );
 
   return (
@@ -200,12 +229,19 @@ export function Home() {
                   variant="dark"
                   label="Ano"
                   placeholder="aaaa"
+                  {...financialEvolutionFilterForm.register('year')}
                 />
-                <ButtonIcon />
+                <ButtonIcon
+                  onClick={financialEvolutionFilterForm.handleSubmit(
+                    onSubmitFinancialEvolution,
+                  )}
+                />
               </ChartAction>
             </header>
             <ChartContent>
-              <FinancialEvolutionBarChart />
+              <FinancialEvolutionBarChart
+                financialEvolution={financialEvolution}
+              />
             </ChartContent>
           </ChartContainer>
         </Section>
