94e41b24c9ba1ddb501ccb474ca95b6f64fb24ec
Samuell Torres <samuelltorres145@gmail.com>
2024-04-18 14:10:46 -0300

    feat(API): metodo de busca de dados da evolucao financeira

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,6 +6,8 @@ import {
   CreateTransaction,
   Dashboard,
   DashboardFilters,
+  FinancialEvolution,
+  FinancialEvolutionFilters,
   Transaction,
   TransactionsFilter,
 } from './api.types';
@@ -78,6 +80,20 @@ export class APIService {
   static async getCategories(): Promise<Category[]> {
     const { data } = await APIService.client.get<Category[]>('/categories');
 
+    return data;
+  }
+
+  static async getFinancialEvolution({
+    year,
+  }: FinancialEvolutionFilters): Promise<FinancialEvolution[]> {
+    const { data } = await APIService.client.get<FinancialEvolution[]>(
+      '/transactions/financial-evolution',
+      {
+        params: {
+          year,
+        },
+      },
+    );
     return data;
   }
 }
